bfa84ab7272f2a0777c6a950ee475bc6906b5cd2
Anton Zelenin <ntnzelenin@gmail.com>
2024-01-28 20:25:17 +0200

    Create project

---
+++ mess_message/models/chat.py
@@ -0,0 +1,32 @@
+from sqlalchemy import Integer, String, ForeignKey, PrimaryKeyConstraint
+from sqlalchemy.orm import mapped_column, Mapped
+
+from mess_message.models import Base
+
+
+class Chat(Base):
+    __tablename__ = 'chats'
+
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, unique=True)
+    name: Mapped[str] = mapped_column(String(255), nullable=False)
+
+
+class ChatMember(Base):
+    __tablename__ = 'chat_members'
+
+    chat_id: Mapped[int] = mapped_column(Integer, ForeignKey('chats.id'))
+    user_id: Mapped[str] = mapped_column(String(32))
+
+    __table_args__ = (
+        PrimaryKeyConstraint('chat_id', 'user_id'),
+    )
+
+
+class Message(Base):
+    __tablename__ = 'messages'
+
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    chat_id: Mapped[int] = mapped_column(Integer, ForeignKey('chats.id'))
+    user_id: Mapped[str] = mapped_column(String(32))
+    text: Mapped[str] = mapped_column(String(255), nullable=False)
+    created_at: Mapped[str] = mapped_column(String(32), nullable=False)
